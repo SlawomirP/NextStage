0ee1066c3195f9f03b9a578090f3f6da19c2fda9
slawo <slawekpettke@gmail.com>
2022-07-13 14:50:10 +0200

    dodanie w mainie ksiazki

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+
+        SimpleDictionary book = new SimpleDictionary();
+
+        book.add("one", "yksi");
+        book.add("two", "kaksi");
+
+        System.out.println(book.translate("one"));
+        System.out.println(book.translate("two"));
+        System.out.println(book.translate("three"));
+
+
+
+
+
+    }
 }
