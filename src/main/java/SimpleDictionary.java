13310dfbdd95de3e997952c2822eb0ce2280f126
slawo <slawekpettke@gmail.com>
2022-07-13 14:46:46 +0200

    dodanie maina uzupelnienie simpleDi

--- src/main/java/SimpleDictionary.java
+++ src/main/java/SimpleDictionary.java
@@ -3,4 +3,17 @@ import java.util.HashMap;
 public class SimpleDictionary {
 
     private HashMap<String, String> translation;
+
+    public SimpleDictionary() {
+        this.translation = new HashMap<>();
+    }
+
+    public String translate(String word) {
+        return this.translation.get(word);
+    }
+
+    public void add(String word, String translation) {
+        this.translation.put(word, translation);
+    }
+
 }
