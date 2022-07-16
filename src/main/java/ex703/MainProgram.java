3bd7ae7c0e0b3020eb200ccd7e81e9db42417196
slawo <slawekpettke@gmail.com>
2022-07-16 19:51:45 +0200

    --

--- src/main/java/ex703/MainProgram.java
+++ src/main/java/ex703/MainProgram.java
@@ -1,44 +1,52 @@
 package ex703;
 
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-        int[] numbers = {-1, 6, 9, 8, 12};
-        System.out.println(MainProgram.indexOfSmallestFrom(numbers, 0));
-        System.out.println(MainProgram.indexOfSmallestFrom(numbers, 1));
-        System.out.println(MainProgram.indexOfSmallestFrom(numbers, 2));
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+//        MainProgram.sort(numbers);
     }
 
     public static int smallest(int[] array) {
         int smallest = array[0];
-        for(Integer number : array){
-            if(number < smallest){
+        for (Integer number : array) {
+            if (number < smallest) {
                 smallest = number;
             }
         }
         return smallest;
     }
-    public static int indexOfSmallest(int[] array){
+
+    public static int indexOfSmallest(int[] array) {
         int smallest = array[0];
         int smallestIndex = 0;
-        for(int i = 0; i < array.length; i++){
-            if(array[i] <= smallest){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] <= smallest) {
                 smallestIndex = i;
             }
         }
         return smallestIndex;
     }
 
-    public static int indexOfSmallestFrom(int[] array, int position){
+    public static int indexOfSmallestFrom(int[] array, int position) {
         int smallest = array[position];
         int smallestIndex = position;
-        for(int i = position; i < array.length; i++){
-            if(array[i] < smallest){
+        for (int i = position; i < array.length; i++) {
+            if (array[i] < smallest) {
                 smallestIndex = i;
             }
         }
         return smallestIndex;
     }
 
-}
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+
+
+    }
---
+++ src/main/java/ex704/Main.java
@@ -0,0 +1,29 @@
+package ex704;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {3, 1, 5, 99, 3, 12};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
+}
